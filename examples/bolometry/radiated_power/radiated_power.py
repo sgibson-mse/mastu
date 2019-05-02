2f7e49020d5d85957d898cc81995b50e4363b6fc
Carr, Matthew <matthew.carr@ukaea.uk>
2019-05-02 14:03:20 +0100

    Corrections to Radiated Power demo, with contributions from @jacklovell and @CnlPepper (#6).

--- examples/bolometry/radiated_power/radiated_power.py
+++ examples/bolometry/radiated_power/radiated_power.py
@@ -2,10 +2,6 @@
 This script reads the result of a SOLPS simulation and calculates the
 total radiated power. It could be used for example to benchmark the
 performance of the bolometer system against simulations.
-
-Currently this script does not work: it requires the implementation
-of cherab.openadas.OpenADAS.stage_resolved_line_radiation_rate and
-cherab.openadas.OpenADAS.stage_resolved_continuum_radiation_rate.
 """
 import numpy as np
 from raysect.optical import World
@@ -14,6 +10,9 @@ from cherab.core.atomic import carbon, deuterium, Isotope
 from cherab.solps import load_solps_from_mdsplus
 from cherab.openadas import OpenADAS
 
+def no_radiation(*_):
+    """Placeholder for when rates are not found"""
+    return 0
 
 world = World()
 adas = OpenADAS(permit_extrapolation=True)  # create atomic data source
@@ -32,8 +31,8 @@ plasma = sim.create_plasma()
 plasma.parent = world
 plasma.atomic_data = adas
 
-sim_te = sim._electron_temperature
-sim_ne = sim._electron_density
+sim_te = sim.electron_temperature
+sim_ne = sim.electron_density
 
 
 total_rad_data = sim.total_radiation
@@ -48,20 +47,30 @@ cherab_total_power = 0
 b2_neutral_i = 0  # counter for B2 neutrals
 for k, species in enumerate(sim.species_list):
 
-    element, ionisation = SPECIES_MAP[species]
+    element, charge = SPECIES_MAP[species]
     if isinstance(element, Isotope):
         element = element.element
-    if element.atomic_number == ionisation:
+    if element.atomic_number == charge:
         continue
 
-    plt = adas.stage_resolved_line_radiation_rate(element, ionisation)
-    prb = adas.stage_resolved_continuum_radiation_rate(element, ionisation)
+    try:
+        plt = adas.line_radiated_power_rate(element, charge)
+    except RuntimeError as ex:
+        print("Line radiation error:", ex)
+        plt = no_radiation
+    try:
+        prb = adas.continuum_radiated_power_rate(element, charge+1)
+    except RuntimeError as ex:
+        print("Continuum radiation error:", ex)
+        prb = no_radiation
 
-    if isinstance(sim.b2_neutral_densities, np.ndarray) and ionisation == 0:
+    if isinstance(sim.b2_neutral_densities, np.ndarray) and charge == 0:
         imp_dens = sim.b2_neutral_densities[:, :, b2_neutral_i]
         b2_neutral_i += 1
     else:
         imp_dens = sim.species_density[:, :, k]
+
+    upper_density = sim.species_density[:, :, k+1]
 
     species_total_power = 0
     for i in range(mesh.nx):
@@ -69,9 +78,10 @@ for k, species in enumerate(sim.species_list):
             ne = sim_ne[i, j]
             te = sim_te[i, j]
             nimp = imp_dens[i, j]
-            species_total_power += (plt(ne, te) * ne * nimp + prb(ne, te) * ne * nimp) * vol[i, j]
+            n_upper = upper_density[i, j]
+            species_total_power += (plt(ne, te) * ne * nimp + prb(ne, te) * ne * n_upper) * vol[i, j]
 
-    print("{} - {} - {:.4G} W".format(element.name, ionisation, species_total_power))
+    print("{} - {} - {:.4G} W".format(element.name, charge, species_total_power))
     cherab_total_power += species_total_power
 
 print("SOLPS total radiated power => {:.4G} W".format(solps_total_power))
